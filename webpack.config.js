b2b2c01f9b00ee3b9e0fc21a4c3e7636059179b7
LAPA\A4YL9ZZ <pxu3@mmm.com>
2017-04-11 10:52:46 +0800

    增加 HtmlWebpackPlugin 练习

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map',//配置生成Source Maps，选择合适的选项	
   entry:  __dirname + "/app/main2.js",//已多次提及的唯一入口文件
   output: {
-    path: __dirname + "/public",//打包后的文件存放的地方
+    path: __dirname + "/build",
     filename: "bundle.js"//打包后输出文件的文件名
   },
   resolveLoader: {
@@ -20,10 +21,11 @@ module.exports = {
 
 
   plugins: [
-    new webpack.BannerPlugin("Copyright Flying Unicorns inc.")//在这个数组中new一个就可以了
+    new HtmlWebpackPlugin({
+      template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+    })
   ],
   devServer: {
-    contentBase: "./public",//本地服务器所加载的页面所在的目录
     colors: true,//终端中输出结果为彩色
     historyApiFallback: true,//不跳转
     inline: true//实时刷新
